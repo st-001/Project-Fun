5a13588cbf134bf796a6b91eef01cc8d2b7ba442
st-001 <120130378+st-00000@users.noreply.github.com>
2023-12-23 12:11:15 +0000

    Add user post schemas

--- routes/users/index.ts
+++ routes/users/index.ts
@@ -1,9 +1,69 @@
 import { Handlers } from "$fresh/server.ts";
-import { getAllUsers, insertUser, type User } from "../../models/user.ts";
+import { getAllUsers, insertUser } from "../../models/user.ts";
 import {
   httpJsonResponse,
   httpResponse500InternalServerError,
 } from "../../utils.ts";
+import ajv from "../../ajv.ts";
+
+export const POST_REQUEST_SCHEMA = {
+  type: "object",
+  properties: {
+    name: { type: "string", maxLength: 255, minLength: 1 },
+    email: { type: "string", format: "email" },
+    password: { type: "string", minLength: 6 },
+    primaryGroupId: { type: "number" },
+  },
+  required: ["name", "email", "password", "primaryGroupId"],
+  additionalProperties: false,
+};
+
+export const POST_RESPONSE_SCHEMA = {
+  type: "object",
+  properties: {
+    id: { type: "number" },
+    name: { type: "string" },
+    email: { type: "string" },
+    primaryGroupId: { type: "number" },
+    isEnabled: { type: "boolean" },
+    createdAt: { type: "string", format: "date-time" },
+    updatedAt: { type: "string", format: "date-time" },
+    deletedAt: {
+      type: ["string", "null"],
+      format: "date-time",
+      nullable: true,
+    },
+    primaryGroup: {
+      type: "object",
+      properties: {
+        id: { type: "number" },
+        name: { type: "string" },
+        isEnabled: { type: "boolean" },
+        createdAt: { type: "string", format: "date-time" },
+        updatedAt: { type: "string", format: "date-time" },
+        deletedAt: {
+          type: ["string", "null"],
+          format: "date-time",
+          nullable: true,
+        },
+      },
+      required: ["id", "name"],
+    },
+  },
+  required: [
+    "id",
+    "name",
+    "email",
+    "primaryGroupId",
+    "isEnabled",
+    "createdAt",
+    "updatedAt",
+    "primaryGroup",
+  ],
+  additionalProperties: false,
+};
+
+const postRequestValidator = ajv.compile(POST_REQUEST_SCHEMA);
 
 export const handler: Handlers = {
   async GET(_req, _ctx) {
@@ -17,12 +77,23 @@ export const handler: Handlers = {
 
   async POST(req, _ctx) {
     try {
-      const user = await req.json() as User;
+      const user = await req.json() as {
+        name: string;
+        email: string;
+        password: string;
+        primaryGroupId: number;
+      };
+      const validRequest = postRequestValidator(user);
+
+      if (!validRequest) {
+        return httpJsonResponse(postRequestValidator.errors, 400);
+      }
+
       const result = await insertUser(
         user.name,
         user.email,
-        user.password!,
-        user.primaryGroupId!,
+        user.password,
+        user.primaryGroupId,
       );
 
       return httpJsonResponse(result, 201);
