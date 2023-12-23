9fcbb37bc8ced65254cbec2fbf33f5d85097f470
st-001 <120130378+st-00000@users.noreply.github.com>
2023-12-23 12:15:32 +0000

    Add reset password request schema validation

--- routes/users/[id]/password.ts
+++ routes/users/[id]/password.ts
@@ -1,10 +1,23 @@
 import { Handlers } from "$fresh/server.ts";
 import { getUserById, resetUserPassword } from "../../../models/user.ts";
 import {
+  httpJsonResponse,
   httpResponse204NoContent,
   httpResponse404NotFound,
   httpResponse500InternalServerError,
 } from "../../../utils.ts";
+import ajv from "../../../ajv.ts";
+
+export const PUT_REQUEST_SCHEMA = {
+  type: "object",
+  properties: {
+    newPassword: { type: "string", minLength: 6 },
+  },
+  required: ["newPassword"],
+  additionalProperties: false,
+};
+
+const putRequestValidator = ajv.compile(PUT_REQUEST_SCHEMA);
 
 export const handler: Handlers = {
   async PUT(req, ctx) {
@@ -14,7 +27,14 @@ export const handler: Handlers = {
         return httpResponse404NotFound();
       }
 
-      const { newPassword } = await req.json() as { newPassword: string };
+      const requestBody = await req.json();
+      const validRequest = putRequestValidator(requestBody);
+
+      if (!validRequest) {
+        return httpJsonResponse(putRequestValidator.errors, 400);
+      }
+
+      const { newPassword } = requestBody as { newPassword: string };
 
       const resetPasswordSuccess = await resetUserPassword(
         user.id,
